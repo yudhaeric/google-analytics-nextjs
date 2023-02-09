0aa22388749df4847f4f99a4febede215052f39c
yudhaeric <yudhaericpamungkas@gmail.com>
2023-02-09 14:02:12 +0700

    Update Homepage

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='mt-[15%]'>
-        <h1 className={`${inter} flex text-4xl justify-center`}>
+        <h1 className={`${inter} flex text-4xl justify-center font-bold`}>
           Testing Google Analytics in<span className='text-[#0070F3]'>&nbsp;Next JS.</span>
         </h1>
         <div className='flex justify-center'>
           <Link href="/form">
-            <button className='px-10 py-3 bg-blue-400 border-2 border-b-[6px] border-solid border-black rounded-lg mt-10 active:border-b-[2px] active:mt-[44px]'>
+            <button className='px-10 py-3 bg-blue-400 border-2 border-b-[6px] border-solid border-black rounded-lg mt-10 active:border-b-[2px] active:mt-[44px]'
+            onClick="ga('send', 'event', 'category', 'action', 'label');"
+            >
               Let&#39;s Go
             </button>
           </Link>
