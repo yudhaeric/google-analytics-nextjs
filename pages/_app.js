8600e62e699241f5353a73877c46783310d83629
yudhaeric <yudhaericpamungkas@gmail.com>
2023-02-09 14:01:01 +0700

    Add Google Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,41 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react'
+import Router from 'next/router'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        ga('send', 'pageview', {
+          page: url,
+          title: 'Homepage'
+        });
+      }
+      Router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        Router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }, [])
+
+    return (
+      <div>
+        <Component {...pageProps} />
+        
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-L7EE4M8YP0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-L7EE4M8YP0');
+          `}
+        </Script>
+      </div>
+    )
+      
+
 }
