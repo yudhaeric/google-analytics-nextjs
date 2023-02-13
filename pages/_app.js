7454a9808b7c453aa6f43dd4811cc1f553f2f0f5
yudhaeric <yudhaericpamungkas@gmail.com>
2023-02-14 03:44:27 +0700

    Update Tag

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
       <div>
         <Component {...pageProps} />
         
-        {/* <Script
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-L7EE4M8YP0"
           strategy="afterInteractive"
         />
@@ -20,15 +20,7 @@ export default function App({ Component, pageProps }) {
 
             gtag('config', 'G-L7EE4M8YP0');
           `}
-        </Script> */}
-
-
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W5CMJ78');</script>
-
+        </Script>
       </div>
     )
 }
