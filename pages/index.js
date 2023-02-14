af089da049de53e847ee4b6173ec36116dbcf083
yudhaeric <yudhaericpamungkas@gmail.com>
2023-02-14 15:04:30 +0700

    Add Event Click

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,25 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    useEffect(() => {
+    const handleClick = () => {
+      gtag("event", "click", {
+        event_category: "Button",
+        event_label: "Let's Go"
+      });
+    };
+
+    const button = document.querySelector("letsgo");
+    button.addEventListener("click", handleClick);
+    return () => {
+      button.removeEventListener("click", handleClick);
+    };
+  }, []);
   return (
     <>
       <Head>
@@ -20,7 +35,7 @@ export default function Home() {
           Testing Google Analytics in<span className='text-[#0070F3]'>&nbsp;Next JS.</span>
         </h1>
         <div className='flex justify-center'>
-          <Link href="/form">
+          <Link id="letsgo" href="/form">
             <button className='px-10 py-3 bg-blue-400 border-2 border-b-[6px] border-solid border-black rounded-lg mt-10 active:border-b-[2px] active:mt-[44px]'
             onClick="ga('send', 'event', 'category', 'action', 'label');"
             >
