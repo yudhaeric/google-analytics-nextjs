bbd6380f448de9e274c0863ba5ad024a50328ca9
yudhaeric <yudhaericpamungkas@gmail.com>
2023-02-14 18:17:52 +0700

    Update Event

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const handleClick = () => {
-    gtag("event", "click", {
-      event_category: "Button",
-      event_label: "Let's Go"
-    });
-  };
+  // const handleClick = () => {
+  //   window.gtag("event", "click", {
+  //     event_category: "Button",
+  //     event_label: "Let's Go"
+  //   });
+  // };
   
   return (
     <>
@@ -29,8 +29,8 @@ export default function Home() {
           Testing Google Analytics in<span className='text-[#0070F3]'>&nbsp;Next JS.</span>
         </h1>
         <div className='flex justify-center'>
-          <Link id="letsgo" href="/form">
-            <button onClick={handleClick()} className='px-10 py-3 bg-blue-400 border-2 border-b-[6px] border-solid border-black rounded-lg mt-10 active:border-b-[2px] active:mt-[44px]'
+          <Link href="/form">
+            <button className='px-10 py-3 bg-blue-400 border-2 border-b-[6px] border-solid border-black rounded-lg mt-10 active:border-b-[2px] active:mt-[44px]'
             >
               Let&#39;s Go
             </button>
